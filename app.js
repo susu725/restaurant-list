b2105262823c5bf688dee03eb5d6257e3b8ca643
susu725 <zxcv1010017@gmail.com>
2023-07-15 02:48:47 +0800

    feat: add edit page and bodyParser & methodOverride

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -13,6 +15,8 @@ const PORT = 2999
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -3,11 +3,29 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
 
+// 瀏覽
 router.get('/:id', (req, res) => {
     const { id } = req.params
     return Restaurant.findOne({ id })
         .lean()
         .then(restaurant => res.render('detail', { restaurant }))
+        .catch(err => console.log(err))
+})
+
+// 編輯
+router.get('/:id/edit', (req, res) => {
+    const { id } = req.params
+    return Restaurant.findOne({ id })
+        .lean()
+        .then(restaurant => res.render('edit', { restaurant }))
+        .catch(err => console.log(err))
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const info = req.body
+    return Restaurant.updateOne({ id: id }, info)
+        .then(() => res.redirect('/'))
         .catch(err => console.log(err))
 })
 
