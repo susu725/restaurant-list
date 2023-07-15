8e3c13bbed4cb86a2815484decb2c55797d7ac10
susu725 <zxcv1010017@gmail.com>
2023-07-15 15:58:05 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -15,6 +16,9 @@ const PORT = 2999
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+    secret: process.env.SESSION_SECRET
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
