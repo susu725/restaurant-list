5cdfa4a3daa630651c83798a89acc0ddb561cb9a
susu725 <zxcv1010017@gmail.com>
2023-07-15 16:26:17 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,4 +11,19 @@ router.get('/register', (req, res) => {
     return res.render('register')
 })
 
+router.post('/register', (req, res) => {
+    const { name, email, password, confirmpassword } = req.body
+    User.findOne({ email }).then(user => {
+        if (user) {
+            console.log('此信箱已被註冊！')
+            res.render('register', { name, email, password, confirmpassword })
+        } else {
+            User.create({ name, email, password })
+                .then(() => res.redirect('/'))
+                .catch(err => console.log(err))
+        }
+    })
+        .catch(err => console.log(err))
+})
+
 module.exports = router
