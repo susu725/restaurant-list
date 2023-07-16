3eac338f0d0f63d900bf566f6883d6c1833e7cbf
susu725 <zxcv1010017@gmail.com>
2023-07-16 18:55:15 +0800

    feat: add isAuthenticated to display defferent view and flash-message

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -26,6 +27,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.warning_msg = req.flash('warning_msg')
+    next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,21 +19,31 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { name, email, password, confirmpassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmpassword) {
+        errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmpassword) {
+        errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
+        return res.render('register', { errors, name, email, password, confirmpassword })
+    }
     User.findOne({ email }).then(user => {
         if (user) {
-            console.log('此信箱已被註冊！')
-            res.render('register', { name, email, password, confirmpassword })
-        } else {
-            User.create({ name, email, password })
-                .then(() => res.redirect('/'))
-                .catch(err => console.log(err))
+            errors.push({ message: '這個 Email 已經註冊過了。' })
+            return res.render('register', { name, email, password, confirmpassword })
         }
+        return User.create({ name, email, password })
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
     })
         .catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
 })
 
